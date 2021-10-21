7d58f53ba30cfab0b2f9ed3eaf96e7c9295190cd
Raju George <raju.george@yahoo.com>
2021-10-21 11:08:13 +0530

    implemented docker code

--- groovy/script.groovy
+++ groovy/script.groovy
@@ -1,5 +1,10 @@
 def buildApp(){
-    echo 'building the application....'
+    echo 'building the image....'
+    withCredentials([usernamePassword(credentialsId: 'nexus-login-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+        sh 'docker build -t 64.227.176.229:8083/movies-app:1.3 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin 64.227.176.229:8083'
+        sh 'docker push 64.227.176.229:8083/movies-app:1.3'
+    }
 }
 
 def testApp(){
